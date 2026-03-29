afd6885a44761cec549763361684bd1a07442456
Ashley Yakeley <ashley@semantic.org>
2026-03-29 00:18:30 -0700

    test: Unlift

--- test/Main.hs
+++ test/Main.hs
@@ -11,9 +11,10 @@ import Coroutine
 import Data
 import Exception
 import Lifecycle
+import Unlift
 
 tests :: TestTree
-tests = testGroup "monadology" [testCoroutine, testLifecycle, testCompose, testException, testData]
+tests = testGroup "monadology" [testCoroutine, testLifecycle, testCompose, testException, testData, testUnlift]
 
 main :: IO ()
 main = defaultMain tests
